0f67c8be5356d3355135f7673ee10210de859db1
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-04-07 17:56:57 -0300

    Integrate with Vokaturi

--- telegram_voice_to_text/config.py
+++ telegram_voice_to_text/config.py
@@ -1,12 +1,16 @@
 from pathlib import Path
 
-
 _config = {}
+
+
+def project_root():
+    return Path(__file__).resolve().parents[1]
+
 
 def get(key):
     global _config
     if not _config:
-        config_file = Path(__file__).resolve().parents[1] / '.env'
+        config_file = project_root() / '.env'
         if not config_file.exists():
             raise RuntimeError('File {} not found'.format(config_file))
         for line in config_file.read_text().splitlines():

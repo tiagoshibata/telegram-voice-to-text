cf4f1dae9a589721b0442e5d0aed54e1e1a5e6ae
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-04-08 00:31:58 -0300

    Add clarifai dep

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
 
     packages=find_packages(),
 
-    install_requires=['google-cloud', 'pytesseract', 'python-telegram-bot', 'scipy'],
+    install_requires=['clarifai', 'google-cloud', 'pytesseract', 'python-telegram-bot', 'scipy'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,

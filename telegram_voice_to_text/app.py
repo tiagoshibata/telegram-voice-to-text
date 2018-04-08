1de788115013374a2f51c2c76c8c138a6715445f
Erich Rodrigues Ferrares <erich.rodrigues@yahoo.com.br>
2018-04-08 06:18:46 -0300

    changes from private reply to reply_markdown

--- telegram_voice_to_text/app.py
+++ telegram_voice_to_text/app.py
@@ -95,7 +95,7 @@ def photo_analysis(bot, update, file_url):
 
     with open('output.jpg', 'r') as handle:
         text = pytesseract.image_to_string(Image.open(handle))
-        private_reply.send_message(bot, u"*Transcrição do texto da imagem:* " + text)
+        update.message.reply_markdown(u"*Transcrição do texto da imagem:* " + text)
 
     app = ClarifaiApp(api_key='d8090e6a90104ec0b190f3a975e5b912')
     model = app.models.get("general-v1.3")
@@ -104,7 +104,7 @@ def photo_analysis(bot, update, file_url):
     text_result = []
     for x in result['outputs'][0]['data']['concepts'][:5]:
         text_result.append(x['name'])
-    private_reply.send_message(bot, u"*Conteúdo da imagem:* " + ', '.join(text_result))
+    update.message.reply_markdown(u"*Conteúdo da imagem:* " + ', '.join(text_result))
 
 
 def photo_handler(bot, update):
